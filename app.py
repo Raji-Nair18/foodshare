043edbbdec78d2c0f053681edbd148caf6cbb065
Raji Nair <nairraji503@email.com>
2026-02-03 20:28:33 +0530

    Final Pages fix

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, session
 import pymysql
 from functools import wraps
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.secret_key = "foodshare_secret"
@@ -182,7 +183,7 @@ def admin():
 if __name__ == '__main__':
     app.run(debug=True)
 
-from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
